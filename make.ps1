dc3fd13ebc83d206847b62361a8288e44936ae8f
arrebole <326093153@qq.com>
2021-06-12 14:31:08 +0800

    重构 make.ps1

--- make.ps1
+++ make.ps1
@@ -2,29 +2,26 @@
 
 $MODE_PATH = "D:\\PlayGames\\Steam\\steamapps\\common\\Don't Starve Together\\mods\\teaparty"
 $MODE_EXPORTED_PATH = $MODE_PATH + "\\exported"
-$COMPILER_PATH = "D:\\PlayGames\\Steam\\steamapps\\common\\Don't Starve Mod Tools\mod_tools\\autocompiler.exe"
 
-function clean {
-     param ($path)
+$TOOLS_PATH = "D:\\PlayGames\\Steam\\steamapps\\common\\Don't Starve Mod Tools\mod_tools\\"
+$TOOLS_AUTOCOMPILER_PATH = $TOOLS_PATH + "autocompiler.exe"
+$TOOLS_PNGCOMPILER_PATH = $TOOLS_PATH + "png.exe"
+$TOOLS_SCMLCOMPILER_PATH = $TOOLS_PATH + "scml.exe"
 
-     if (-Not (Test-Path $path))
-     {
-          return
-     }
-     Remove-Item $path -Recurse -Force
+
+#  如果没有mod文件夹则创建
+if (-Not (Test-Path $MODE_PATH))
+{
+     mkdir -path $MODE_PATH
 }
+# 将当前目录下的文件安装到游戏mod目录下
+Remove-Item ($MODE_PATH + "\*") -Recurse
+Copy-Item "./*" -Destination $MODE_PATH -Recurse
 
-function copyCache {
-     if (-Not (Test-Path $MODE_PATH))
-     {
-          mkdir -path $MODE_PATH
-     }
-     Copy-Item "./*" -Destination $MODE_PATH -Recurse
-     
+# 编译
+Start-Process -FilePath $TOOLS_AUTOCOMPILER_PATH -NoNewWindow -Wait
+
+# 清理需要动画编译前的源文件
+if (Test-Path $MODE_EXPORTED_PATH) {
+     Remove-Item $MODE_EXPORTED_PATH -Recurse -Force
 }
-
-function build {
-     Start-Process -FilePath $COMPILER_PATH -NoNewWindow -Wait
-}
-
-clean $MODE_PATH && copyCache && build && clean $MODE_EXPORTED_PATH
